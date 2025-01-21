a13693983096464b55864751e24c4cbccb206394
AitorLopez057 <alopezbarr1@educacion.navarra.es>
2025-01-21 20:05:33 +0100

    Creación de carpeta de servicios

---
+++ src/Services/ActivityTypeService.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Service;
+
+class ActivityTypeService
+{
+    public function __construct()
+    {
+        // Constructor de servicio (si se necesitan dependencias, se añaden aquí)
+    }
+
+    // Aquí puedes añadir los métodos de tu servicio
+}
